85aca6dd93f5a0580c43c69a847b2256e6021bfc
mavis <rxm1279@miami.edu>
2019-06-24 12:41:41 -0400

    add traing/test data for logistic regression

--- Burns.R
+++ Burns.R
@@ -26,14 +26,14 @@ stat.desc(burn1000$tbsa, basic = TRUE, desc = TRUE, norm = FALSE, p=0.95)
 
 
 ## graphics for all variables
-gghistogram(burn1000, x = "age", bins = 100, 
+gghistogram(burn1000, x = "age", bins = 500, 
             add = "median")
 
 
 gghistogram(burn1000, x = "id", bins = 100, 
             add = "mean")
 
-gghistogram(burn1000, x = "tbsa", bins = 100, 
+gghistogram(burn1000, x = "tbsa", bins = 10000, 
             add = "mean")
 
 ggplot(burn1000, aes(x = gender)) +
@@ -55,3 +55,40 @@ ggplot(burn1000, aes(x = inh_inj)) +
 ggplot(burn1000, aes(x = flame)) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+## Build a logistic regression model
+head(burn1000)
+table(burn1000$death)
+
+# create training data
+input_dead <- burn1000[which(burn1000$death == "Dead"), ]
+input_alive <- burn1000[which(burn1000$death == "Alive"), ]
+set.seed(100)  # for repeatability of samples
+input_dead_trainRows <- sample(1:nrow(input_dead), 0.7 * nrow(input_dead))
+input_alive_trainRows <- sample(1:nrow(input_alive), 0.7 * nrow(input_dead))
+training_dead <- input_dead[input_dead_trainRows, ]
+training_alive <- input_alive[input_alive_trainRows, ]
+burn_train <- rbind(training_dead, training_alive) #row bind the Alive and Dead
+
+# create test data
+test_dead <- input_dead[-input_dead_trainRows, ]
+test_alive <- input_alive[-input_alive_trainRows, ]
+burn_test <- rbind(test_dead, test_alive)
+
+
+
+
+glm.fit <- glm(death ~ age + gender + race + tbsa + inh_inj + flame,
+               data = burn1000,
+               family = binomial)
+
+
+
+
+
+
+
+
+
+
